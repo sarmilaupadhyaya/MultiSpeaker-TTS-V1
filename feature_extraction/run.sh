238adcda24adf46196f07d3e6cb0e9b228b35196
sarmilaupadhyaya <sarmilaupadhyaya@outlook.com>
2021-12-06 09:23:58 +0100

    adds new data

--- feature_extraction/run.sh
+++ feature_extraction/run.sh
@@ -4,17 +4,22 @@
 
 source ../newenv/bin/activate
 
-python main.py siwis_val.txt fr 0 0
-python main.py siwis_test.txt fr 0 0
-python main.py siwis_train.txt fr 0 0
-python main.py ljs_train.txt en 1 1
-python main.py ljs_test.txt en 1 1
-python main.py ljs_val.txt en 1 1
-python main.py VCTK_data_2.txt en 1 2
-python main.py VCTK_data_3.txt en 1 3
-python main.py VCTK_data_4.txt en 1 4
-python main.py VCTK_data_5.txt en 1 5
-python main.py VCTK_data_6.txt en 1 6
+#python main.py siwis_val.txt fr 0 0
+#python main.py siwis_test.txt fr 0 0
+#python main.py siwis_train.txt fr 0 0
+#python main.py ljs_train.txt en 1 1
+#python main.py ljs_test.txt en 1 1
+#python main.py ljs_val.txt en 1 1
+#python main.py VCTK_data_2.txt en 1 2
+#python main.py VCTK_data_3.txt en 1 3
+#python main.py VCTK_data_4.txt en 1 4
+#python main.py VCTK_data_5.txt en 1 5
+#python main.py VCTK_data_6.txt en 1 6
 #python main.py vctk_train.txt en 2 1 
+python main.py  text_chevalier.txt fr 0 7
+python main.py text_hugo.txt fr 0 8
+python main.py text_sue.txt fr 0 9
+python main.py text_tundra fr 0 10
+python main.py text_zola fr 0 11
 
 #python train.py config_speaker.json
